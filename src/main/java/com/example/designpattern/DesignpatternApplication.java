b6c6d1da90e9368a2e2102f2f669c067d1ff96b8
xandersu <xandersu666@gmail.com>
2018-11-18 19:05:31 +0800

    add uml知识

--- src/main/java/com/example/designpattern/DesignpatternApplication.java
+++ src/main/java/com/example/designpattern/DesignpatternApplication.java
@@ -3,6 +3,18 @@ package com.example.designpattern;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * java设计模式精讲 Debug 方式+内存分析
+ * start:
+ * 设计模式定义 - 设计模式应用 - 源码解析 - 系统学习设计模式
+ * 动态递进式讲解 场景-UML-代码 - 采用debug方式及内存分析 - 理解设计模式 - jdk及热门开源框架源码 - 领略设计模式技巧 - 学懂、面试
+ * UML图、7大设计原则
+ * 创建型模式
+ * 结构式模式
+ * 行为型模式
+ *
+ *
+ */
 @SpringBootApplication
 public class DesignpatternApplication {
 
---
+++ src/main/java/com/example/designpattern/designdiscipline/DesignDiscipline.java
@@ -0,0 +1,9 @@
+package com.example.designpattern.designdiscipline;
+
+/**
+ * @Author: suxun
+ * @Date: 2018/11/18 18:56
+ * @Description: 设计原则
+ */
+public interface DesignDiscipline {
+}
